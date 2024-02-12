610189732053d66a9b6ef279a8de51eb03162ace
Jair Torezone <jairtorezone@gmail.com>
2024-02-12 17:14:48 -0300

    Components Tag

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,6 +3,7 @@ import { Container, Links } from "./styles";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
 
 export function Details() {
   return (
@@ -15,9 +16,14 @@ export function Details() {
             <a href="#">https://www.rocketseat.com.br</a>
           </li>
           <li>
-            <a href="#">https://www.rocketseat.com.br</a>
+            <a href="#">https://www.rocketseat.com.br</a>{" "}
           </li>
         </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="nodejs" />
       </Section>
 
       <Button title="Voltar" />
