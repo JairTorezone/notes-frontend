3e78c0b0114fd1f12f0fa262de8251ef796ca398
Jair Torezone <jairtorezone@gmail.com>
2024-02-12 12:32:25 -0300

    Components

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,9 +1,11 @@
+import { Button } from "../../components/Button";
 import { Container } from "./styles";
 
 export function Details() {
   return (
     <Container>
       <h1>Ola mundo</h1>
+      <Button />
     </Container>
   );
 }
