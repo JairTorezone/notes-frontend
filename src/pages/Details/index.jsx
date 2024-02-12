e939f1951932976223e1c7f11bd23d754d1e4172
Jair Torezone <jairtorezone@gmail.com>
2024-02-12 17:33:59 -0300

    finishing details page

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from "./styles";
+import { Container, Links, Content } from "./styles";
 
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
@@ -10,25 +10,40 @@ export function Details() {
   return (
     <Container>
       <Header />
-      <ButtonText title="Excluir notas" />
 
-      <Section title="Links úteis">
-        <Links>
-          <li>
-            <a href="#">https://www.rocketseat.com.br</a>
-          </li>
-          <li>
-            <a href="#">https://www.rocketseat.com.br</a>{" "}
-          </li>
-        </Links>
-      </Section>
+      <main>
+        <Content>
+          <ButtonText title="Excluir notas" />
 
-      <Section title="Marcadores">
-        <Tag title="express" />
-        <Tag title="nodejs" />
-      </Section>
+          <h1>Introdução ao React</h1>
 
-      <Button title="Voltar" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, sequi
+            ducimus! Suscipit adipisci sunt sit, aperiam veritatis magnam culpa
+            ipsa autem eligendi, facere iure sequi sapiente. Nesciunt laborum
+            commodi rem?ipsa autem eligendi, facere iure sequi sapiente.
+            Nesciunt laborum commodi rem?
+          </p>
+
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="#">https://www.rocketseat.com.br</a>
+              </li>
+              <li>
+                <a href="#">https://www.rocketseat.com.br</a>{" "}
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="nodejs" />
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   );
 }
