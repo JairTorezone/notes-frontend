3308654918da83272529d89173bc929d90a5fdfa
Jair Torezone <jairtorezone@gmail.com>
2024-02-12 20:17:48 -0300

    Profile

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,40 @@
+import { FiArrowLeft, FiMail, FiUser, FiLock, FiCamera } from "react-icons/fi";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+import { Container, Form, Avatar } from "./styles";
+
+export function Profile() {
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <FiArrowLeft />
+        </button>
+      </header>
+
+      <Form>
+        <Avatar>
+          {/* <img src={avatar} alt="Foto do usuario" /> */}
+
+          <label htmlFor="avatar">
+            <FiCamera />
+
+            <input id="avatar" type="file" />
+          </label>
+        </Avatar>
+
+        <Input placeholder="Nome" type="text" icon={FiUser} />
+
+        <Input placeholder="E-mail" type="text" icon={FiMail} />
+
+        <Input placeholder="Senha atual" type="password" icon={FiLock} />
+
+        <Input placeholder="Nova senha" type="password" icon={FiLock} />
+
+        <Button title="Salvar" />
+      </Form>
+    </Container>
+  );
+}
