43ca64f5cfceeaf7eb3f28baa1fccd04cf04fe81
Jair Torezone <jairtorezone@gmail.com>
2024-03-17 20:06:37 -0300

    update project

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,23 +1,37 @@
 import { RiShutDownLine } from "react-icons/ri";
 import { Container, Profile, Logout } from "./styles";
 
+import { api } from "../../services/api";
+import avatarPlaceholder from "../../assets/avatar_placeholder.svg";
+
 import { useAuth } from "../../hooks/auth";
+import { useNavigate } from "react-router-dom";
 
 export function Header() {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
+  const navigation = useNavigate();
+
+  function handleSignOut() {
+    navigation("/");
+    signOut();
+  }
+
+  const avatarUrl = user.avatar
+    ? `${api.defaults.baseURL}/files/${user.avatar}`
+    : avatarPlaceholder;
 
   return (
     <Container>
       <Profile to="/profile">
-        <img src="https://github.com/jairtorezone.png" alt="Foto do usuário" />
+        <img src={avatarUrl} alt={user.name} />
 
         <div>
           <span>Bem-vindo</span>
-          <strong>Jair Torezone</strong>
+          <strong> {user.name} </strong>
         </div>
       </Profile>
 
-      <Logout onClick={signOut}>
+      <Logout onClick={handleSignOut}>
         <RiShutDownLine />
       </Logout>
     </Container>
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,11 +1,53 @@
 import { FiArrowLeft, FiMail, FiUser, FiLock, FiCamera } from "react-icons/fi";
 import { Link } from "react-router-dom";
+
+import { useAuth } from "../../hooks/auth";
+
+import { api } from "../../services/api";
+import avatarPlaceholder from "../../assets/avatar_placeholder.svg";
+
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 
 import { Container, Form, Avatar } from "./styles";
 
+import { useState } from "react";
+
 export function Profile() {
+  const { user, updateProfile } = useAuth();
+
+  const [name, setName] = useState(user.name);
+  const [email, setEmail] = useState(user.email);
+  const [passwordOld, setPasswordOld] = useState();
+  const [passwordNew, setPasswordNew] = useState();
+
+  const avatarUrl = user.avatar
+    ? `${api.defaults.baseURL}/files/${user.avatar}`
+    : avatarPlaceholder;
+
+  const [avatar, setAvatar] = useState(avatarUrl);
+  const [avatarFile, setAvatarFile] = useState(null);
+
+  async function handleUpdate() {
+    const updated = {
+      name,
+      email,
+      password: passwordNew,
+      old_password: passwordOld,
+    };
+    const userUpdated = Object.assign(user, updated);
+
+    await updateProfile({ user: userUpdated, avatarFile });
+  }
+
+  function handleChangeAvatar(event) {
+    const file = event.target.files[0];
+    setAvatarFile(file);
+
+    const imagePreview = URL.createObjectURL(file);
+    setAvatar(imagePreview);
+  }
+
   return (
     <Container>
       <header>
@@ -16,27 +58,46 @@ export function Profile() {
 
       <Form>
         <Avatar>
-          <img
-            src="https://github.com/jairtorezone.png"
-            alt="Foto do usuario"
-          />
+          <img src={avatar} alt="Foto do usuario" />
 
           <label htmlFor="avatar">
             <FiCamera />
 
-            <input id="avatar" type="file" />
+            <input id="avatar" type="file" onChange={handleChangeAvatar} />
           </label>
         </Avatar>
 
-        <Input placeholder="Nome" type="text" icon={FiUser} />
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
 
-        <Input placeholder="E-mail" type="text" icon={FiMail} />
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-        <Input placeholder="Senha atual" type="password" icon={FiLock} />
+        <Input
+          placeholder="Senha atual"
+          type="password"
+          icon={FiLock}
+          onChange={(e) => setPasswordOld(e.target.value)}
+        />
 
-        <Input placeholder="Nova senha" type="password" icon={FiLock} />
+        <Input
+          placeholder="Nova senha"
+          type="password"
+          icon={FiLock}
+          onChange={(e) => setPasswordNew(e.target.value)}
+        />
 
-        <Button title="Salvar" />
+        <Button title="Salvar" onClick={handleUpdate} />
       </Form>
     </Container>
   );
